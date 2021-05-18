318b4280b8351c8f47438f26985e4057947d2419
Kirill <you@example.com>
2021-05-18 18:02:49 +0400

    first commit

---
+++ secondpage/secondpagejs.js
@@ -0,0 +1,42 @@
+
+const right = document.getElementsByClassName('right')[0];
+const notright = document.getElementsByClassName('notright')[0];
+right.onclick = function(){
+	right.style.backgroundColor = '#00A550';
+}
+notright.onclick = function(){
+	notright.style.backgroundColor = '#FE3A3A';
+}
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn1");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn2");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
